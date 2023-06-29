6c998806ade04def58a386565acb7053e93521bd
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-29 17:31:13 +0900

    GAN-9 feat: item조회 기능 추가

--- src/main/java/com/mutsa/mutsamarket/domain/salesitem/repository/SalesItemRepository.java
+++ src/main/java/com/mutsa/mutsamarket/domain/salesitem/repository/SalesItemRepository.java
@@ -1,8 +1,11 @@
 package com.mutsa.mutsamarket.domain.salesitem.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mutsa.mutsamarket.domain.salesitem.entity.SalesItem;
 
 public interface SalesItemRepository extends JpaRepository<SalesItem, Long> {
+	Page<SalesItem> findAllPages(Pageable pageable);
 }
