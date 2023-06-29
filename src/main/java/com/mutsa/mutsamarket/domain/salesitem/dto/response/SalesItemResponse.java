d0e91d12dae36a7779b131ed302b6153cbfc4284
GangminRYOU <dbrkdals1994@gmail.com>
2023-06-29 17:32:52 +0900

    GAN-9 feat: 반환 객체 필드 수정

--- src/main/java/com/mutsa/mutsamarket/domain/salesitem/dto/response/SalesItemResponse.java
+++ src/main/java/com/mutsa/mutsamarket/domain/salesitem/dto/response/SalesItemResponse.java
@@ -1,14 +1,24 @@
 package com.mutsa.mutsamarket.domain.salesitem.dto.response;
 
+import com.mutsa.mutsamarket.domain.salesitem.entity.Status;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class SalesItemResponse {
-	private String message;
+	private String title;
+	private String description;
+	private Integer minPriceWanted;
+	private Status status;
 
 	@Builder
-	public SalesItemResponse(String message) {
-		this.message = message;
+	public SalesItemResponse(String title, String description, Integer minPriceWanted, Status status) {
+		this.title = title;
+		this.description = description;
+		this.minPriceWanted = minPriceWanted;
+		this.status = status;
 	}
 }
