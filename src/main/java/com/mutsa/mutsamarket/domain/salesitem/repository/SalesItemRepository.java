f499ef87f595cdc59a3ec50201e06f596d7fb4f1
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-01 16:12:36 +0900

    GAN-10 fix: 버전 업데이트에 따른도메인 리포지토리 페이징 오류 수정

--- src/main/java/com/mutsa/mutsamarket/domain/salesitem/repository/SalesItemRepository.java
+++ src/main/java/com/mutsa/mutsamarket/domain/salesitem/repository/SalesItemRepository.java
@@ -3,9 +3,10 @@ package com.mutsa.mutsamarket.domain.salesitem.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.mutsa.mutsamarket.domain.salesitem.entity.SalesItem;
 
-public interface SalesItemRepository extends JpaRepository<SalesItem, Long> {
-	Page<SalesItem> findAllPages(Pageable pageable);
+public interface SalesItemRepository extends JpaRepository<SalesItem, Long>, PagingAndSortingRepository<SalesItem, Long> {
+	Page<SalesItem> findAll(Pageable pageable);
 }
