61c3fc5df372420444514460b19894a9ada190f2
GangminRYOU <dbrkdals1994@gmail.com>
2023-07-03 12:57:35 +0900

    GAN-11 feat: 인증 관련 에러 코드 추가

--- src/main/java/com/mutsa/mutsamarket/common/exception/ErrorCode.java
+++ src/main/java/com/mutsa/mutsamarket/common/exception/ErrorCode.java
@@ -8,10 +8,15 @@ import lombok.Getter;
 public enum ErrorCode {
 
 	ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "I_001", "상품을 찾을 수 없습니다."),
-	DUPLICATED_ITEM_ERROR(HttpStatus.BAD_REQUEST, "I_002", "이미 등록된 상품입니다."),
+	AUTHENTICATION_FAILURE(HttpStatus.BAD_REQUEST, "A_001", "인증에 실패했습니다."),
 
+	DUPLICATED_ITEM_ERROR(HttpStatus.BAD_REQUEST, "I_002", "이미 등록된 상품입니다."),
+	WRONG_PASSWORD_ERROR(HttpStatus.BAD_REQUEST, "I_003", "비밀번호가 틀렸습니다."),
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S_002", "서버에 장애가 있습니다."),
 
 	HASHING_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "S_001", "해싱에 실패하였습니다.");
+
+
 
 	private final HttpStatus httpStatus;
 	private final String errorCode;
